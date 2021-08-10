da33cb82a16bf544dcf47a034cba429133fad592
Коба Владимир (Философия-ИТ) <vkoba@fil-it.ru>
2021-08-10 12:30:33 +0300

    Initial commit

---
+++ src/test/java/ru/inleksis/MvideoHighloadServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.inleksis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvideoHighloadServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
